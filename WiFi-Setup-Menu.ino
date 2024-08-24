2d9494c0499f1bdc244926768e2b68c4bcbf6dae
RobZ-se <154233599+RobZ-se@users.noreply.github.com>
2024-08-24 23:04:33 +0200

    Update WiFi-Setup-Menu.ino
    Cleanup

--- WiFi-Setup-Menu.ino
+++ WiFi-Setup-Menu.ino
@@ -1,13 +1,10 @@
-/**
- * WiFiManager
- */
+/** 
+ * configuration of WiFi using a Captive Portal
+ * edit the file wm_strings_en.h in Arduino/libraries/WiFiManager to change HTML style, reply strings and more
+ **/
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
-#define TRIGGER_PIN 0
 
-// wifimanager can run in a blocking mode or a non blocking mode
-// Be sure to know how to process loops with no delay() if using non blocking
 bool wm_nonblocking = false; // change to true to use non blocking
-
 WiFiManager wm; // global wm instance
 WiFiManagerParameter custom_field; // global param ( for non blocking w params )
 
@@ -15,87 +12,23 @@ void  WiFi_init() {
   WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP  
   Serial.setDebugOutput(true);  
   delay(3000);
-  Serial.println("\n Starting");
-  pinMode(TRIGGER_PIN, INPUT);
-  // wm.resetSettings(); // uncomment to wipe WiFi settings
+  Serial.println("\n Starting WiFi Acess Point in captive portal mode");
+// wm.resetSettings(); // ----  erase stored WiFi settings (This is for testing purposes. Comment out this line in production)
+// ---- COMMENT OUT ABOVE LINE  -------
   if(wm_nonblocking) wm.setConfigPortalBlocking(false);
-  // add a custom input field
-  int customFieldLength = 40;
-  // new (&custom_field) WiFiManagerParameter("customfieldid", "Custom Field Label", "Custom Field Value", customFieldLength,"placeholder=\"Custom Field Placeholder\"");
-  // test custom html input type(checkbox)
-  // new (&custom_field) WiFiManagerParameter("customfieldid", "Custom Field Label", "Custom Field Value", customFieldLength,"placeholder=\"Custom Field Placeholder\" type=\"checkbox\""); // custom html type
-  // test custom html(radio)
-  const char* custom_radio_str = "<br/><label for='customfieldid'>Custom Field Label</label><input type='radio' name='customfieldid' value='1' checked> One<br><input type='radio' name='customfieldid' value='2'> Two<br><input type='radio' name='customfieldid' value='3'> Three";
-  new (&custom_field) WiFiManagerParameter(custom_radio_str); // custom html input
-  wm.addParameter(&custom_field);
-  wm.setSaveParamsCallback(saveParamCallback);
-
-  // custom menu via array or vector
-  // menu tokens, "wifi","wifinoscan","info","param","close","sep","erase","restart","exit" (sep is seperator) (if param is in menu, params will not show up in wifi page!)
-  // const char* menu[] = {"wifi","info","param","sep","restart","exit"}; 
-  // wm.setMenu(menu,6);
-  std::vector<const char *> menu = {"wifi","info","param","sep","restart","exit"};
+  std::vector<const char *> menu = {"wifi"};
   wm.setMenu(menu);
-  wm.setClass("invert");// set dark theme
-
-  //set static ip
-  // wm.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0)); // set static ip,gw,sn
-  //wm.setShowStaticFields(true); // force show static ip fields
-  //wm.setShowDnsFields(true);    // force show dns field always
-  // wm.setConnectTimeout(20); // how long to try to connect for before continuing
-  wm.setConfigPortalTimeout(30); // auto close configportal after n seconds
-  // wm.setCaptivePortalEnable(false); // disable captive portal redirection
-  wm.setAPClientCheck(true); // avoid timeout if client connected to softap
-  // wifi scan settings
-  // wm.setRemoveDuplicateAPs(false); // do not remove duplicate ap names (true)
-  // wm.setMinimumSignalQuality(20);  // set min RSSI (percentage) to show in scans, null = 8%
-  // wm.setShowInfoErase(false);      // do not show erase button on info page
-  // wm.setScanDispPerc(true);       // show RSSI as percentage not graph icons
-  // wm.setBreakAfterConfig(true);   // always exit configportal even if wifi save fails
-  // res = wm.autoConnect(); // auto generated AP name from chipid
-  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-  bool con = wm.autoConnect("WSPR-TX_XP_Setup"); // non password protected ap
-  if(!con) {
-    Serial.println("Failed to connect before timeout");
-    //ESP.restart();
+  wm.setClass("invert");
+  wm.setShowStaticFields(true); // force show static ip fields
+  wm.setShowDnsFields(true);    // force show dns field always
+  wm.setConfigPortalTimeout(300); // auto close configportal after n seconds
+  // wm.autoConnect("AutoConnectAP","password"); // password protected ap
+  if(wm.autoConnect("WSPR-TX-XP")) {//SSID Name of Captive portal 
+    Serial.print("connecteded to WiFi network- ");
+    Serial.println(wm.getWiFiSSID());
   } 
-  else {
-    //if you get here you have connected to the WiFi    
-    Serial.println("connected to WiFi");
-  }
+  else {Serial.println("Failed to connect or hit timeout");}
 }
-
-// void checkButton(){
-//   // check for button press
-//   if ( digitalRead(TRIGGER_PIN) == LOW ) {
-//     // poor mans debounce/press-hold, code not ideal for production
-//     delay(50);
-//     if( digitalRead(TRIGGER_PIN) == LOW ){
-//       Serial.println("Button Pressed");
-//       // still holding button for 3000 ms, reset settings, code not ideaa for production
-//       delay(3000); // reset delay hold
-//       if( digitalRead(TRIGGER_PIN) == LOW ){
-//         Serial.println("Button Held");
-//         Serial.println("Erasing Config, restarting");
-//         wm.resetSettings();
-//         ESP.restart();
-//       }
-      
-//       // start portal w delay
-//       Serial.println("Starting config portal");
-//       wm.setConfigPortalTimeout(120);
-      
-//       if (!wm.startConfigPortal("OnDemandAP","password")) {
-//         Serial.println("failed to connect or hit timeout");
-//         delay(3000);
-//         // ESP.restart();
-//       } else {
-//         //if you get here you have connected to the WiFi
-//         Serial.println("Connected to WiFi network.)");
-//       }
-//     }
-//   }
-// }
 
 
 String getParam(String name){
@@ -111,10 +44,6 @@ void saveParamCallback(){
   Serial.println("[CALLBACK] saveParamCallback fired");
   Serial.println("PARAM customfieldid = " + getParam("customfieldid"));
 }
-
-//if(wm_nonblocking) wm.process(); // avoid delays() in loop when non-blocking and other long running code  
-  //checkButton();
-  // put your main code here, to run repeatedly:
 
 
 void resetWiFi(){
