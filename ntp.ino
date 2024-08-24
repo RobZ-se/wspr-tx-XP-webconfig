3b803cb8dd7eb858a2f9b383c425d1d9e7a48a7e
RobZ-se <154233599+RobZ-se@users.noreply.github.com>
2024-08-24 23:02:50 +0200

    Update ntp.ino
    Cleanup

--- ntp.ino
+++ ntp.ino
@@ -3,20 +3,13 @@
 bool iniNTP(){
   Serial.println("NTP starting.\nFetching date and time from pool.ntp.org");
   timeClient.begin();// Initialize a NTPClient to get time
-  // Set offset time in seconds to adjust for your timezone, for example:
-  // GMT +1 = 3600
-  // GMT +8 = 28800
-  // GMT -1 = -3600
-  // GMT 0 = 0
   timeClient.setTimeOffset(0);
   timeClient.update();
   time_t epochTime = timeClient.getEpochTime();
   Serial.print("Epoch Time: ");
   Serial.println(epochTime);
-  
   String formattedTime = timeClient.getFormattedTime();
-  Serial.print("Time is ");
+  Serial.print("UTC time:");
   Serial.println(formattedTime);  
-  
   return timeClient.isTimeSet();
 }
